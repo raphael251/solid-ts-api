4e30cb348e49e968ddabf0110ce39a2fc291db41
Raphael Passos <raphael251@hotmail.com>
2020-07-30 22:25:48 -0300

    chore: add post users route

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { createUserController } from "./useCases/CreateUser";
+
+const router = Router()
+
+router.post('/users', (req, res) => {
+    return createUserController.handle(req, res)
+})
+
+export { router }
